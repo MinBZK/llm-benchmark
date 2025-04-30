b564cedf860605968d8dafb5a6733c9612449e20
Anne Schuth <anne.schuth@rijksoverheid.nl>
2025-04-30 14:49:59 +0200

    fix import

--- benchmarks/social-bias/generate-prompts/prompts.py
+++ benchmarks/social-bias/generate-prompts/prompts.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import sys
+import os
 
+# Add the current directory to the path so we can import Template
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from Template import Template
 
 
